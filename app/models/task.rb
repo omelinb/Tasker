3143ff61761dc6b6d894ef303d63e4ac82eee690
elros <elros.ob@gmail.com>
2022-07-13 20:23:33 +0300

    Add slim; devise/turbo bug fix; devise/views customization

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  STATUSES = %w[new in_progress completed canceled].freeze
+
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id', required: true
   has_many :approvals, dependent: :destroy
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+  end
 
-  root 'tasks#index'
+  resources :tasks, only: %i[index]
+
+  root to: 'tasks#index'
 end
