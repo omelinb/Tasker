1de1c5e4062780c060172c7dff6ffdb3673aa67c
elros <elros.ob@gmail.com>
2022-07-18 01:19:37 +0300

    Add show task view

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :tasks, only: %i[index new create] do
+  resources :tasks, only: %i[index show new create] do
     resources :approvals, only: %i[create]
   end
 
